6105991b245178adcb8b251277107dfba3d642fc
Yuji(YJ) Toyama <ytoyama@google.com>
2021-10-19 10:44:57 +0900

    test!

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("version1!-CI/CD-Cloud Run Hello, world. You're at the polls index.")
+    return HttpResponse("version1!-CI/CD-Cloud Run Hello!!, world. You're at the polls index.")
 
